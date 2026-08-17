b34e56035acb74d419c7cb46380c079b1fdbd9e7
vikas kumhar <vikaskumhar811@gmail.com>
2026-08-17 12:15:11 +0530

    add meeting module 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-
-// @Module({
-//   imports: [],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 
 
 import { Module } from '@nestjs/common';
@@ -20,17 +9,12 @@ import { AuthModule } from './auth/auth.module';
 import { TeachersModule } from './teachers/teachers.module';
 import { ParentsModule } from './parents/parents.module';
 import { StudentsModule } from './students/students.module';
-import { MeetingsService } from './meetings/meetings.service';
-import { MeetingsController } from './meetings/meetings.controller';
 import { MeetingsModule } from './meetings/meetings.module';
 
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   isGlobal: true,
-    //   load: [typeorm],
-    // }),
+
     ConfigModule.forRoot({
   isGlobal: true,
   load: [typeorm],
@@ -56,42 +40,10 @@ import { MeetingsModule } from './meetings/meetings.module';
     StudentsModule,
     MeetingsModule,
   ],
-  // providers: [MeetingsService],
-  // controllers: [MeetingsController],
+
 })
 export class AppModule {}
 
 
 
 
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: 'localhost',
-//       port: 5432,
-//       username: 'postgres',
-//       password: 'password',
-//       database: 'nestdb',
-//       autoLoadEntities: true,
-//       synchronize: true,
-//     }),
-//   ],
-// })
-// export class AppModule {}
-
-
-
-
-// npm install @nestjs/typeorm typeorm pg
-// npm install @nestjs/config
-// npm install class-validator class-transformer
-// npm install @nestjs/jwt @nestjs/passport passport passport-jwt
-// npm install bcrypt
-// npm install @nestjs/swagger
-
-
-
-
-// npm install dotenv
-// npm install -D ts-node
